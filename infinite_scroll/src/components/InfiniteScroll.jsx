8f75fdad04d1a1eaf37fa83c8872f3e29c4edf42
Maria Clara Duarte <mcduarte9@hotmail.com>
2025-07-23 10:13:55 -0300

    feat: setup inicial

---
+++ infinite_scroll/src/components/InfiniteScroll.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const InfiniteScroll = () => {
+    return (
+        <div>
+            <h1>Infinite Scroll</h1>
+            <ul>
+                <li>
+                    <h3>Título</h3>
+                    <p>Corpo</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default InfiniteScroll
